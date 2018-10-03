27480dfef037a7bc0323d32f2e5225ffbd0345c2
surnan <forsuresh@yandex.com>
2018-10-03 08:26:41 -0400

    Successfully runs without storyboard

--- Podcast_Player/Podcast_Player/FirstViewController.swift
+++ Podcast_Player/Podcast_Player/FirstViewController.swift
@@ -8,11 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FirstViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = UIColor.yellow
+        
+        
     }
 
 
