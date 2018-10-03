711c6c8005fbb3ca5b57a30645ebaf443f30042f
surnan <forsuresh@yandex.com>
2018-10-03 08:21:57 -0400

    Project Created

---
+++ Podcast_Player/Podcast_Player/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Podcast_Player
+//
+//  Created by admin on 10/3/18.
+//  Copyright © 2018 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
